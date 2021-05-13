20db3f6abf5ebde157e216b0ba55cc20da8f2931
themoon8947 <fz5soul@gmail.com>
2021-05-13 19:59:17 +0700

    demo1

---
+++ js/slick.js
@@ -0,0 +1,9 @@
+$('.slick__content').slick({
+    infinite: true,
+    slidesToShow: 2,
+    autoplay: true,
+    autoplaySpeed: 800,
+    speed:800,           
+    dots: true,
+    arrows: false
+});
